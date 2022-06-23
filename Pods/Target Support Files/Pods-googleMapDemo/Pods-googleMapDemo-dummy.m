d5c7cf01fbffd414a609136064ccf85119a9f597
ramansaini95 <deep65399@gmail.com>
2022-06-23 09:44:11 +0530

    initial Commit

---
+++ Pods/Target Support Files/Pods-googleMapDemo/Pods-googleMapDemo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_googleMapDemo : NSObject
+@end
+@implementation PodsDummy_Pods_googleMapDemo
+@end
--- googleMapDemo/ViewController.swift
+++ googleMapDemo/ViewController.swift
@@ -6,14 +6,54 @@
 //
 
 import UIKit
+import GoogleMaps
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet fileprivate weak var mapView: GMSMapView!
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let camera = GMSCameraPosition.camera(withLatitude:30.713329, longitude: 76.695566, zoom: 6.0)
+        mapView.camera = camera
+        showMarker(position: camera.target)
+        
+        self.locationManager.requestAlwaysAuthorization()
+        
+        // For use in foreground
+        self.locationManager.requestWhenInUseAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.startUpdatingLocation()
+        }
     }
-
-
+    
+    func showMarker(position: CLLocationCoordinate2D){
+        let marker = GMSMarker()
+        marker.position = position
+        marker.title = "Rabab"
+        marker.snippet = "Mohali"
+        marker.map = mapView
+    }
+}
+extension ViewController: GMSMapViewDelegate{
+    func mapView(_ mapView: GMSMapView, didBeginDragging marker: GMSMarker) {
+        print("didBeginDragging")
+    }
+    func mapView(_ mapView: GMSMapView, didDrag marker: GMSMarker) {
+        print("didDrag")
+    }
+    func mapView(_ mapView: GMSMapView, didEndDragging marker: GMSMarker) {
+        print("didEndDragging")
+    }
 }
 
+extension ViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let locValue: CLLocationCoordinate2D = manager.location?.coordinate else { return }
+        print("locations = \(locValue.latitude) \(locValue.longitude)")
+    }
+}
